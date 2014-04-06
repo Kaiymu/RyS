f99a4d7bcda6e3933142a084b1105eb11dcf8846
FONTAINE_THOMAS <thomas.fontaine.tf@gmail.com>
2014-04-06 17:54:47 +0200

    Ajout d'une skyboxe, et tentative de compréhension du script de musique >.<

---
+++ Assets/Scripts/MusicManager/MusicManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    private string path = Application.dataPath + "/../Music/";
+
+    public struct MusicInfo
+    {
+        public string name;
+        public string fullPath;
+    }
+
+    private static MusicManager mInstance;
+    public static MusicManager Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                AudioListener audioListener = GameObject.FindObjectOfType<AudioListener>();
+                if (audioListener == null)
+                {
+                    GameObject o = new GameObject("MusicManager");
+                    audioListener = o.AddComponent<AudioListener>();
+                }
+                mInstance = audioListener.gameObject.AddComponent<MusicManager>();
+                mInstance.mAudioSource = audioListener.gameObject.GetComponent<AudioSource>();
+                if (mInstance.mAudioSource == null)
+                    mInstance.mAudioSource = audioListener.gameObject.AddComponent<AudioSource>();
+            }
+            return mInstance;
+        }
+    }
+
+    private AudioSource mAudioSource;
+    public AudioSource AudioSource
+    {
+        get { return this.mAudioSource; }
+    }
+
+    public MusicInfo[] GetMusicList()
+    {
+        System.Collections.Generic.List<MusicInfo> musicInfo = new System.Collections.Generic.List<MusicInfo>();
+        string[] fullPath = System.IO.Directory.GetFiles(this.path, "*.ogg", System.IO.SearchOption.TopDirectoryOnly);
+        foreach (string path in fullPath)
+        {
+            MusicInfo info = new MusicInfo();
+            info.fullPath = path;
+            info.name = path.Substring(path.LastIndexOf('/')+1, path.LastIndexOf('.') - path.LastIndexOf('/') - 1);
+            musicInfo.Add(info);
+        }
+        return musicInfo.ToArray();
+    }
+
+    public void StartMusic(string path)
+    {
+        StartCoroutine(OpenAndPlay(path));
+    }
+
+    private IEnumerator OpenAndPlay(string path)
+    {
+        WWW www = new WWW("file://" + path);
+        this.AudioSource.clip = www.GetAudioClip(false, true);
+        while (this.AudioSource.clip.isReadyToPlay == false) yield return null;
+        this.AudioSource.Play();
+    }
+}
---
+++ Assets/Scripts/MusicManager/TestMusicManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMusicManager : MonoBehaviour {
+
+	
+    void OnGUI()
+    {
+        MusicManager.MusicInfo[] music = MusicManager.Instance.GetMusicList();
+        if (MusicManager.Instance.AudioSource.isPlaying)
+        {
+            if (GUILayout.Button("Pause")) 
+                MusicManager.Instance.AudioSource.Pause();
+        }
+        else if (MusicManager.Instance.AudioSource.clip!= null && MusicManager.Instance.AudioSource.clip.isReadyToPlay)
+        {
+            if (GUILayout.Button("Play")) 
+                MusicManager.Instance.AudioSource.Play();
+        }
+        
+        foreach (MusicManager.MusicInfo musicName in music)
+        {
+            if (GUILayout.Button(musicName.name))
+                MusicManager.Instance.StartMusic(musicName.fullPath);
+        }
+    }
+}
