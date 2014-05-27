fdf2eca371ca60a7e478bdaaaa7c8f1e865f7319
breizhpanda <kieffer41@hotmail.fr>
2014-05-27 06:01:10 +0200

    menu entree
    - plat
    - dessert

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GoToPlay() {
+
+		Application.LoadLevel("test");
+	}
+}
